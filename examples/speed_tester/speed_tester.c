0a3443fd714bac258f37fa97302f2f11b1763a66
Ananth <ab7325@nyu.edu>
2018-05-07 20:53:01 -0400

    speed tester CI

--- examples/speed_tester/speed_tester.c
+++ examples/speed_tester/speed_tester.c
@@ -400,11 +400,11 @@ int main(int argc, char *argv[]) {
                         ehdr->ether_type = LOCAL_EXPERIMENTAL_ETHER;
 
 			struct ci_hdr ci;
-			ci.sender = 1
-			ci.recipient = 2
-			ci.subject = 1
-			ci.attributes = 1
-			ci.tp = 1
+			ci.sender = 1;
+			ci.recipient = 2;
+			ci.subject = 1;
+			ci.attributes = 1;
+			ci.tp = 1;
 			struct ci_hdr *ci_ptr = onvm_pkt_ci_hdr(pkt);
 			ci_ptr = &ci;
                         pmeta = onvm_get_pkt_meta(pkt);
